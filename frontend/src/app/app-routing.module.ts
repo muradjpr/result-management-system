890d6c58221cb384fe8c58451b42dcb46bcafbb7
hmmurad <muradjpr@gmail.com>
2023-02-26 14:44:26 +0600

    After adding PrimeNg Table...

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './components/auth/auth.component';
+import { AddResultComponent } from './components/add-result/add-result.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: '' , redirectTo: '/home', pathMatch: 'full'},
+  { path : '', redirectTo: '/login', pathMatch: 'full'},
   { path : 'home', component: HomeComponent},
-  { path: 'auth', component: AuthComponent},
-  { path: '**', component: NotFoundComponent}
+  { path : 'add-result', component: AddResultComponent},
+  { path : 'register', component: RegisterComponent},
+  { path : 'login', component: LoginComponent},
+  { path : '**', component: NotFoundComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http'
+
+import {CardModule} from 'primeng/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-import { AuthComponent } from './components/auth/auth.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {DividerModule} from 'primeng/divider';
+import {ButtonModule} from 'primeng/button';
+import {StyleClassModule} from 'primeng/styleclass';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import {MenubarModule} from 'primeng/menubar';
+import { AddResultComponent } from './components/add-result/add-result.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    AuthComponent,
-    NotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeComponent,
+        NotFoundComponent,
+        RegisterComponent,
+        LoginComponent,
+        AddResultComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        CardModule,
+        InputTextModule,
+        PasswordModule,
+        DividerModule,
+        ButtonModule,
+        StyleClassModule,
+        MenubarModule
+    ],
+    providers: [],
+    exports: [
+        HeaderComponent
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
